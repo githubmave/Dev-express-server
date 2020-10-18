6fdea98049c01303ad51dc0e4ea511756a591fc0
githubmave <lidanmei@yahoo.com>
2020-10-18 22:13:11 +1300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,31 @@ const server=require('./server.js');
 //});
 
 
-server.get('/profile/:name', (req, res) => {
+server.get('/profiles',(req,res)=>
+{
+   res.sendFile(__dirname +'/profiles.html');               
+});
+
+    switch(req.params.name) {
+
+
+server.get('/profiles/:id,(req, res) => {
+    console.log(req.params)
+
+    switch(req.params.id) {
+        case '1':
+            return res.sendFile(__dirname + '/silvia.html')
+
+       case '2':
+            return res.sendFile(__dirname + '/sampson.html')
+
+        default:
+            res.sendFile(__dirname + '/silvia.html')
+    }
+
+
+
+server.post('/profile/:name', (req, res) => {
     console.log(req.params)
 
     switch(req.params.name) {
