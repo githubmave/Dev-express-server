9e0246ef94fd8e79fd26f9847588664d64b45c21
githubmave <lidanmei@yahoo.com>
2020-10-18 22:53:36 +1300

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,21 @@ server.get('/profiles/:id,(req, res) => {
         default:
             res.sendFile(__dirname + '/profiles.html')
     }
+        
+        
+        server.get('/profiles/:id,(req, res) => {
+    console.log(req.params)
+
+    switch(req.params.id) {
+        case '1':
+            return res.sendFile(__dirname + '/public/silvia.html')
+
+       case '2':
+            return res.sendFile(__dirname + '/public/sampson.html')
+
+        default:
+            res.sendFile(__dirname + '/profiles.html')
+    }
 
 
 
