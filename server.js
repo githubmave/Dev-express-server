a6c5f6e8a0660d54c5fea6fd4d00ed18bf845086
githubmave <lidanmei@yahoo.com>
2020-10-18 20:46:43 +1300

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const express=require('express');
 
 const server=express();
 
+server.get('/compliment',(req,res)=>
+{
+   res.send('<h>you did a good job</h>');               
+});
+
+
+server.listen(3000,()=>{
+   console.log('localhost listening in port 3000')              
+})
+
 module.exports=server;
